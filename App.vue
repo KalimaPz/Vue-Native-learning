1b2fd388523ffdb608fb2276f4d3ecb60b08a1fc
KalimaPz <kalima007c@gmail.com>
2020-09-16 02:56:14 +0700

    :tada: Initial Vue Native Project first time
    Description Vue learned faster than React

---
+++ App.vue
@@ -0,0 +1,44 @@
+<template>
+  <view class="container">
+    <text class="text-color-primary">My Vue Native App</text>
+    <text>Hello Phone</text>
+    <text>{{ count }}</text>
+    <text>{{ name }}</text>
+    <view>
+      <button
+        title="Increase"
+        :on-press="increment"
+      />
+      <text-input
+      :style="{height : 40 , width : 80}"
+      v-model="name"
+      />
+    </view>
+  </view>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      count : 0,
+      name : ''
+    }
+  },
+  methods: {
+    increment() {
+      this.count++
+    }
+  },
+}
+</script>
+<style>
+.container {
+  background-color: white;
+  align-items: center;
+  justify-content: center;
+  flex: 1;
+}
+.text-color-primary {
+  color: blue;
+}
+</style>
